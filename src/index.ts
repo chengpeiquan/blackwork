4634cb7c1df8ceb59fa95384105421ae8230ab99
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-07-14 01:25:36 +0800

    chore: adjust export rules

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@
 
 import './styles/globals.css'
 
-export * from './components/ui/button'
-export * from './components/ui/dropdown-menu'
-export * from './components/ui/pagination'
-
 export * from './components/theme'
+export * from './components/ui'
 
 export * from './utils'
