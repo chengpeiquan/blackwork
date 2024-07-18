f3d3f83ed7b9c8464b48a1bc99a82df1c220fddd
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-07-18 23:07:55 +0800

    feat(layout-main): add fullscreen mode

--- src/components/layouts/layout-main.tsx
+++ src/components/layouts/layout-main.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { cn } from '@/utils'
 
 export interface LayoutMainProps {
+  fullscreen?: boolean
   className?: string
   children: React.ReactNode
 }
 
 export const LayoutMain: React.FC<LayoutMainProps> = ({
+  fullscreen = false,
   className,
   children,
 }) => {
   const cls = cn(
-    'flex flex-col grow flex-shrink-0 container box-border px-6 sm:px-8',
+    'flex flex-col grow flex-shrink-0 box-border',
+    fullscreen ? 'w-screen' : 'container px-6 sm:px-8',
     className,
   )
 
