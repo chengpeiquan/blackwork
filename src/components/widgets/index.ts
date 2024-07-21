66a97f50108935a3bd15387d68e7e9c79c8dfd8a
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-07-21 19:28:52 +0800

    feat(language-toggle): add language toggle

--- src/components/widgets/index.ts
+++ src/components/widgets/index.ts
@@ -1,4 +1,5 @@
 export * from './external-link'
+export * from './language-toggle'
 export * from './social-link'
 export * from './social-links'
 export * from './typography'
---
+++ src/components/widgets/language-toggle.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import { isObject, isUndefined } from '@bassist/utils'
+import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui'
+import * as Icons from './icons'
+
+export interface LanguageToggleOptions {
+  value: string
+  label?: React.ReactNode
+  icon?: React.ReactNode
+  onClick?: (value: string) => void
+}
+
+export interface LanguageToggleProps {
+  title?: string
+  ariaLabel?: string
+  defaultValue?: string
+  options: LanguageToggleOptions[] | LanguageToggleOptions
+}
+
+const isSingle = (v: unknown): v is LanguageToggleOptions => isObject(v)
+
+const DefaultIcon: React.FC = () => (
+  <Icons.Language className="h-5 w-5 rotate-0 scale-100 transition-all" />
+)
+
+export const LanguageToggle: React.FC<LanguageToggleProps> = ({
+  title,
+  ariaLabel,
+  defaultValue,
+  options,
+}) => {
+  const icon = useMemo(() => {
+    if (!isUndefined(defaultValue)) {
+      const currentOption = isSingle(options)
+        ? options
+        : options?.find((i) => i.value === defaultValue)
+      if (!currentOption) return <DefaultIcon />
+
+      return currentOption?.icon || <DefaultIcon />
+    }
+
+    return <DefaultIcon />
+  }, [defaultValue, options])
+
+  if (isSingle(options)) {
+    return (
+      <Button
+        variant="ghost"
+        size="icon"
+        title={title}
+        aria-label={ariaLabel || title}
+        onClick={() => options.onClick?.(options.value)}
+      >
+        {icon}
+        <span className="sr-only">Toggle Language</span>
+      </Button>
+    )
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          title={title}
+          aria-label={ariaLabel || title}
+        >
+          {icon}
+          <span className="sr-only">Toggle Language</span>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end">
+        {options.map((i) => {
+          return (
+            <DropdownMenuItem
+              key={i.value}
+              className="gap-2"
+              defaultChecked={i.value === defaultValue}
+              onClick={() => i.onClick?.(i.value)}
+            >
+              {i.label}
+            </DropdownMenuItem>
+          )
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
