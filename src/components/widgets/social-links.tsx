b424a81cd7a5eff0aba8e8e700451528886bdf9f
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-09-29 22:52:18 +0800

    feat(social-links): add custom class name

--- src/components/widgets/social-links.tsx
+++ src/components/widgets/social-links.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { isArray } from '@bassist/utils'
 import { SocialLink, type SocialLinkProps } from './social-link'
+import { cn } from '@/utils'
 
 export interface SocialLinksProps {
   items: SocialLinkProps[]
+  className?: string
 }
 
-export const SocialLinks: React.FC<SocialLinksProps> = ({ items }) => {
+export const SocialLinks: React.FC<SocialLinksProps> = ({
+  items,
+  className,
+}) => {
+  const cls = cn('flex flex-shrink-0 items-center gap-1', className)
+
   if (!isArray(items) || !items.length) return null
   return (
-    <div className="flex flex-shrink-0 items-center gap-1">
+    <div className={cls}>
       {items.map((i) => {
         return <SocialLink key={i.link} {...i} />
       })}
