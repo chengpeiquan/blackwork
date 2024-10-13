3322177858b61e1d4d847b684202dc2cb6d37f07
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-10-13 22:23:37 +0800

    feat(card): add card

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,7 @@
 export * from './aspect-ratio'
 export * from './avatar'
 export * from './button'
+export * from './card'
 export * from './collapsible'
 export * from './drawer'
 export * from './dropdown-menu'
