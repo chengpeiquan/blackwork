294d37654cfec941947e0225521b9b0a0cf36453
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-10-16 23:16:33 +0800

    feat(toast): add toast

---
+++ src/components/ui/toast.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React from 'react'
+import { Toaster as Sonner, toast } from 'sonner'
+import { type Theme, useTheme } from '../theme'
+
+type ToasterProps = React.ComponentProps<typeof Sonner>
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = 'dark' } = useTheme()
+
+  return (
+    <Sonner
+      theme={theme as Theme}
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+          description: 'group-[.toast]:text-muted-foreground',
+          actionButton:
+            'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
+          cancelButton:
+            'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
+        },
+      }}
+      {...props}
+    />
+  )
+}
+
+export { Toaster, toast }
