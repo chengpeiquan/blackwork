3063a9193f112053be9d59778b0cdccca7676960
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-12-23 00:56:42 +0800

    fix(sheet): using wrong close icon

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import * as SheetPrimitive from '@radix-ui/react-dialog'
 import { type VariantProps, cva } from 'class-variance-authority'
 
-import { X } from '@/icons'
+import { Close } from '@/icons'
 import { cn } from '@/utils'
 
 const Sheet = SheetPrimitive.Root
@@ -66,7 +66,7 @@ const SheetContent = React.forwardRef<
     >
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <X className="h-4 w-4" />
+        <Close className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
     </SheetPrimitive.Content>
