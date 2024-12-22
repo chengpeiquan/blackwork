63cc03232b8c1ad85ace479d3e120e44a28bbd62
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-12-22 19:51:21 +0800

    feat(icons): add icons

---
+++ src/icons/sound-off.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { type IconProps } from './types'
+
+export const SoundOff: React.FC<IconProps> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeWidth="2"
+      >
+        <path d="m22 15l-6-6m6 0l-6 6"></path>
+        <path
+          strokeLinejoin="round"
+          d="M2 14.959V9.04C2 8.466 2.448 8 3 8h3.586a.98.98 0 0 0 .707-.305l3-3.388c.63-.656 1.707-.191 1.707.736v13.914c0 .934-1.09 1.395-1.716.726l-2.99-3.369A.98.98 0 0 0 6.578 16H3c-.552 0-1-.466-1-1.041"
+        ></path>
+      </g>
+    </svg>
+  )
+}
---
+++ src/icons/sound-on.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { type IconProps } from './types'
+
+export const SoundOn: React.FC<IconProps> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M2 14.959V9.04C2 8.466 2.448 8 3 8h3.586a.98.98 0 0 0 .707-.305l3-3.388c.63-.656 1.707-.191 1.707.736v13.914c0 .934-1.09 1.395-1.716.726l-2.99-3.369A.98.98 0 0 0 6.578 16H3c-.552 0-1-.466-1-1.041M16 8.5c1.333 1.778 1.333 5.222 0 7M19 5c3.988 3.808 4.012 10.217 0 14"
+      ></path>
+    </svg>
+  )
+}
